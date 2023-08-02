56296041b9933e376c3eef4b82210ba120bfef54
ChiragJindal21 <82804902+ChiragJindal21@users.noreply.github.com>
2023-08-02 12:22:40 +0530

    Adding c# file

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace class1app1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Chirag";
+            int age = 102;
+            string line = $"My name is {name}. " +
+$"My age is {age}.";
+            Console.WriteLine(line);
+        }
+    }
+}
