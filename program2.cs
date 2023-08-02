70a14e81de954d11b5c84ed1ec42cef2b277c2a3
ChiragJindal21 <chiragjindal45331@gmail.com>
2023-08-02 12:46:22 +0530

    this is my first commit - Chirag

---
+++ program2.cs
@@ -0,0 +1,14 @@
+namespace class1app1
+{
+    internal class program2
+    {
+        static void Main(string[] args)
+        {
+            string name = "ChiragJindal";
+            int age = 103;
+            string line = $"My name is {name}. " +
+$"My age is {age}.";
+            Console.WriteLine(line);
+        }
+    }
+}
