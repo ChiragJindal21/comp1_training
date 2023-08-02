e639861a399d75a49eb342cbe06a76430126117f
ChiragJindal21 <chiragjindal45331@gmail.com>
2023-08-02 12:53:34 +0530

    second commit from Chirag

--- program2.cs
+++ program2.cs
@@ -4,6 +4,7 @@ namespace class1app1
     {
         static void Main(string[] args)
         {
+            // Making changes again
             string name = "ChiragJindal";
             int age = 103;
             string line = $"My name is {name}. " +
